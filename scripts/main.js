a1f525bd0707f360fb0afeacaa9c26a1ad18b55b
user8595 <muhikhsansyakir@gmail.com>
2025-12-25 15:35:31 +0800

    change navbar script

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,26 @@
 // navbar
-const navB = document.getElementById("navbar");
-let screenYOff = window.scrollY;
-window.onscroll = function () {
+document.addEventListener("DOMContentLoaded", () => {
   const navB = document.getElementById("navbar");
-  let currScrY = window.scrollY;
-  if (screenYOff > currScrY) {
-    navB.style.top = "0";
-  } else {
-    navB.style.top = "-68px";
-  }
-  screenYOff = currScrY;
-};
-
-document.addEventListener("DOMContentLoaded", function() {
+  let screenYOff = window.scrollY;
+  let navDelay = false;
+  document.addEventListener("scroll", () => {
     const navB = document.getElementById("navbar");
-    navB.style.position = "sticky"
+    const navBox = navB.getBoundingClientRect();
+    let navH = navBox.height;
+    let currScrY = window.scrollY;
+    if (!navDelay) {
+      // delay on scroll
+      setTimeout(() => {
+        if (screenYOff > currScrY) {
+          navB.style.top = "0";
+        } else {
+          navB.style.top = `-${navH}px`;
+        }
+        screenYOff = currScrY;
+        navDelay = false;
+      }, 45);
+      navDelay = true;
+    }
+  });
+  navB.style.position = "sticky";
 });
