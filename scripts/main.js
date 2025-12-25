ab694104706a91b13ccc8190504feb2b4fddccc7
user8595 <muhikhsansyakir@gmail.com>
2025-12-25 22:38:26 +0800

    remove boolean in code

--- scripts/main.js
+++ scripts/main.js
@@ -2,25 +2,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const navB = document.getElementById("navbar");
   let screenYOff = window.scrollY;
-  let navDelay = false;
   document.addEventListener("scroll", () => {
     const navB = document.getElementById("navbar");
     const navBox = navB.getBoundingClientRect();
     let navH = navBox.height;
     let currScrY = window.scrollY;
-    if (!navDelay) {
-      // delay on scroll
-      setTimeout(() => {
-        if (screenYOff > currScrY) {
-          navB.style.top = "0";
-        } else {
-          navB.style.top = `-${navH}px`;
-        }
-        screenYOff = currScrY;
-        navDelay = false;
-      }, 45);
-      navDelay = true;
-    }
+    // delay on scroll
+    setTimeout(() => {
+      if (screenYOff > currScrY) {
+        navB.style.top = "0";
+      } else {
+        navB.style.top = `-${navH}px`;
+      }
+      screenYOff = currScrY;
+    }, 45);
   });
   navB.style.position = "sticky";
 });
